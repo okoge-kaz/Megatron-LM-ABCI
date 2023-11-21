08d113d6c5f502fd411580f71ae4c8a4577ca79e
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-21 15:29:56 +0900

    feat: parallel corpus tokenize

--- scripts/abci/tokenize/tokenize.sh
+++ scripts/abci/tokenize/tokenize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -l rt_AF=1
-#$ -l h_rt=5:00:00
+#$ -l h_rt=15:00:00
 #$ -j y
 #$ -o outputs/tokenize/
 #$ -cwd
@@ -13,34 +13,34 @@ module load nccl/2.16/2.16.2-1
 module load hpcx/2.12
 
 # python virtualenv
-cd /bb/llm/gaf51275/llama/Megatron-LM
+cd /bb/llm/gaf51275/llama/Megatron-LM-freeze
 source .env/bin/activate
 
-DATASET_DIR=/bb/llm/gaf51275/llama/datasets/llama2-llm-jp-corpus/v1.0.2/sample
-OUTPUT_DIR=/bb/llm/gaf51275/llama/datasets/llama2-llm-jp-corpus/v1.0.2/tokenized/sentencepiece
+DATASET_DIR=/bb/llm/gaf51275/jalm/JParaCrawl3.0
+OUTPUT_DIR=/bb/llm/gaf51275/llama/datasets/JParaCrawl3.0
+
+TOKENIZER_MODEL=/bb/llm/gaf51275/jalm/jalm-tokenizer-private/tokenizer/jalm_llama_okazaki_lab_cc_nfkc_16k_aligned_8/merged_tokenizer_sp/jalm_llama.model
 
 mkdir -p ${OUTPUT_DIR}
 
-# tokenize japanese cc
-for ((i = 0; i <= 37; i++)); do
-  INPUT_FILE=${DATASET_DIR}/ja_cc/merged_train_${i}.jsonl
+# tokenize
+INPUT_FILE=${DATASET_DIR}/default_plain_text_format.jsonl
 
-  python tools/preprocess_data.py \
-    --input ${INPUT_FILE} \
-    --output-prefix ${OUTPUT_DIR}/ja_cc_${i} \
-    --tokenizer-type Llama2Tokenizer \
-    --tokenizer-model /bb/llm/gaf51275/jalm/jalm-tokenizer-private/tokenizer/jalm_llama_clueweb/merged_tokenizer_sp/jalm_llama.model \
-    --vocab-file /bb/llm/gaf51275/jalm/jalm-tokenizer-private/tokenizer/jalm_llama_clueweb/merged_tokenizer_sp/jalm_llama.vocab \
-    --append-eod \
-    --workers 64
-done
-
-# tokenize japanese wikipedia
 python tools/preprocess_data.py \
-  --input ${DATASET_DIR}/ja_wiki/merged_train_0.jsonl \
-  --output-prefix ${OUTPUT_DIR}/ja_wiki \
+  --input ${INPUT_FILE} \
+  --output-prefix ${OUTPUT_DIR}/default_plain_text_format \
   --tokenizer-type Llama2Tokenizer \
-  --tokenizer-model /bb/llm/gaf51275/jalm/jalm-tokenizer-private/tokenizer/jalm_llama_clueweb/merged_tokenizer_sp/jalm_llama.model \
-  --vocab-file /bb/llm/gaf51275/jalm/jalm-tokenizer-private/tokenizer/jalm_llama_clueweb/merged_tokenizer_sp/jalm_llama.vocab \
+  --tokenizer-model ${TOKENIZER_MODEL} \
+  --append-eod \
+  --workers 64
+
+# tokenize
+INPUT_FILE=${DATASET_DIR}/highquality_plain_text_format.jsonl
+
+python tools/preprocess_data.py \
+  --input ${INPUT_FILE} \
+  --output-prefix ${OUTPUT_DIR}/highquality_plain_text_format \
+  --tokenizer-type Llama2Tokenizer \
+  --tokenizer-model ${TOKENIZER_MODEL} \
   --append-eod \
   --workers 64
