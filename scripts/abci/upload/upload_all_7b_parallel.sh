5982ddc2333b4d3758fd43ba769a12e789a031b3
kazuki <kaz.tokyo.tech20@gmail.com>
2023-11-25 23:26:24 +0900

    chore: update checkpoint

--- scripts/abci/upload/upload_all_7b_parallel.sh
+++ scripts/abci/upload/upload_all_7b_parallel.sh
@@ -6,12 +6,12 @@ start=5000
 end=15000
 increment=5000
 
-upload_base_dir=/bb/llm/gaf51275/llama/from_megatron_hf_checkpoints/hf_checkpoints/Llama2-7b-base-parallel/shuffle-high-quality-next-token
+upload_base_dir=/bb/llm/gaf51275/llama/from_megatron_hf_checkpoints/hf_checkpoints/Llama2-7b-base-parallel/shuffle-normal-quality-next-token
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
 
   python scripts/abci/upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Llama2-7b-base-parallel-corpus-next-token-shuffle-high-quality-megatron-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Llama2-7b-base-parallel-corpus-next-token-shuffle-normal-quality-megatron-iter$(printf "%07d" $i)
 done
